f4972c318875f2056d47d417e7a80242a2e79afc
ilhwan.park <ihpark92@gmail.com>
2018-03-24 11:03:43 +0900

    Add private key conversion code

---
+++ privatekey.cpp
@@ -0,0 +1,48 @@
+#include <bitcoin/bitcoin.hpp>
+#include <string.h>
+#include <iostream>
+
+using namespace bc;
+
+int main(void)
+{
+	data_chunk seed(16);
+	pseudo_random_fill(seed);
+	ec_secret secretKey = bitcoin_hash(seed);
+
+	std::string hexKey = encode_base16(secretKey);;
+	std::cout << "Hex secret: " << std::endl;
+	std::cout << hexKey << std::endl;
+
+	ec_secret rawprivateKey;
+	decode_base16(rawprivateKey, hexKey);
+	wallet::ec_private privateKey(rawprivateKey, 0x8000, false);
+	std::cout << "\nPrivate Key Structure: " << std::endl;
+	std::cout << privateKey.encoded() << std::endl;
+	ec_secret keyTest = privateKey.secret();
+	std::string keyTestString = encode_base16(keyTest);
+	if(keyTestString == hexKey){
+	    	std::cout << "Confirmed: " <<keyTestString << std::endl;
+	}
+	else{
+	    	std::cout << "Error!" <<keyTestString << std::endl;
+	}
+
+	hexKey = "80" + hexKey;
+	std::cout << "\nVersioned Key: " << std::endl;
+	std::cout << hexKey << std::endl;
+
+	data_chunk fullKey;
+	data_chunk wifVersion;
+	byte_array<37u> versionedKey;
+
+	decode_base16(fullKey, hexKey);
+	decode_base16(wifVersion, "0x80");
+	build_checked_array(versionedKey, { wifVersion, fullKey});
+
+	std::string practiceKey = encode_base58(versionedKey);
+	std::cout<< "\nBy Hand:" << std::endl;
+	std::cout<< practiceKey << std::endl;
+}
+
+
