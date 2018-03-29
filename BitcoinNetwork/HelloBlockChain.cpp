a65b3f7e51b7d8a916282117e4b3af03ef62af1b
ilhwan.park <ihpark92@gmail.com>
2018-03-29 21:34:22 +0900

    Add server connection test code

---
+++ BitcoinNetwork/HelloBlockChain.cpp
@@ -0,0 +1,40 @@
+#include <bitcoin/bitcoin.hpp>
+#include <bitcoin/client.hpp>
+#include <string.h>
+#include <iostream>
+#include <cstdint>
+ 
+using namespace bc;
+
+int main()
+{
+	client::connection_type connection = {};
+	connection.retries = 3;
+	connection.timeout_seconds = 8;
+	connection.server = config::endpoint("tcp://mainnet.libbitcoin.net:9091");
+
+	const auto on_reply = [](size_t blockHeight) 
+	{
+		std::cout << "Height: " << blockHeight << std::endl;
+
+	};
+
+	static const auto on_error = [](const code& ec) 
+	{
+
+		std::cout << "Error Code: " << ec.message() << std::endl;
+
+	};
+
+	client::obelisk_client client(connection);
+
+	if(!client.connect(connection))
+	{
+		std::cout << "Fail" << std::endl;
+	} else {
+		std::cout << "Connection Succeeded" << std::endl;
+	}
+
+	client.blockchain_fetch_last_height(on_error, on_reply);
+	client.wait();
+}
